9fce2f59e13b7736fe8e68210fba4c03dcde9767
zahrarmz <68514679+zahrarmz@users.noreply.github.com>
2021-09-01 23:50:55 +0430

    change time to local time.

--- src/app.js
+++ src/app.js
@@ -6,3 +6,33 @@ function searchCity(event) {
 }
 let input = document.querySelector("form");
 input.addEventListener("submit", searchCity);
+
+function changeTime() {
+  let now = new Date();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "	Saturday",
+  ];
+  let currentDay = days[now.getDay()];
+
+  let currentHours = now.getHours();
+  let currentMinutes = now.getMinutes();
+
+  let date = document.querySelector("#currentTime");
+  let time = document.querySelector("#time");
+  if (currentMinutes < 10) {
+    date.innerHTML = `${currentDay}`;
+    time.innerHTML = `${currentHours}: 0${currentMinutes} `;
+  } else {
+    date.innerHTML = `${currentDay}`;
+    time.innerHTML = `${currentHours}: ${currentMinutes} `;
+  }
+}
+
+changeTime();
